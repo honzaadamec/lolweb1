d1a38debb4d9766704d16ad7bf483bb53343c2f8
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-22 04:12:31 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+
+  <title></title>
+
+  </head>
+  <body>
+    <header>
+      <nav>
+        <a href="#">
+        </a>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Turnaje</a></li>
+          <li><a href="#">Kontakty</a></li>
+        </ul>
+        <div>
+          <form action="includes/login.inc.php" method="post"> // post method je pro secure, je to cast kterou uzivatel nevidi, budou v nem scripty jakou jsou login logout atd..
+            <input type="text" name="mailuid" placeholder="E-mail...">
+            <input type="password" name="pwd" placeholder="password...">
+            <button type="submit" name="login-submit">Login</button>
+          </form>
+        <a href="signup.php">Signup</a>
+          <form action="includes/logout.inc.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+          </form>
+      </nav>
+    </header>
+  </body>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require "header.php";
+ ?>
+
+      <main>
+        <p>Jsi odhlasen</p>
+        <p>Jsi prihlasen</p>
+      </main>
+
+ <?php
+     require "footer.php";
+  ?>
