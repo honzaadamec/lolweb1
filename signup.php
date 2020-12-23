a62f41f943310c6701c33e8987326ec5207640da
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-23 18:48:31 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 
       <main>
         <h1>Signup</h1>
-        <form action="includes/signup.inc.php" method="post">
+        <form action="signup.inc.php" method="post">
           <input type="text" name="uid" placeholder="Username">
           <input type="text" name="mail" placeholder="E-mail">
           <input type="password" name="pwd" placeholder="Password">
