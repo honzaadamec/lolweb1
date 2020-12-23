08a1a078749327edc5d5d0bf219f77230c69c0e0
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-23 18:01:04 +0100

    Add files via upload

--- Includes/signup.inc.php
+++ Includes/signup.inc.php
@@ -3,13 +3,13 @@ if (isset($_POST['signup-submit'])) {
 
 require 'dbh.inc.php';
 
-$username = $_POST['uid']
-$email = $_POST['mail']
-$password = $_POST['pwd']
-$passwordRepeat = $_POST['pwd-repeat']
+$username = $_POST['uid'];
+$email = $_POST['mail'];
+$password = $_POST['pwd'];
+$passwordRepeat = $_POST['pwd-repeat'];
 
-  if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)){
-    require '../signup.php';
+ if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)){
+    header("Location: ../signup.php?error=emptyfields&".$username."&mail=".$email);
     exit();
   }
   else if (){
