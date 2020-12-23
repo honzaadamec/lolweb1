f842b6f90c4b93eae1ff8032885814096d55c461
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-23 17:54:13 +0100

    Add files via upload

--- Includes/signup.inc.php
+++ Includes/signup.inc.php
@@ -9,7 +9,7 @@ $password = $_POST['pwd']
 $passwordRepeat = $_POST['pwd-repeat']
 
   if (empty($username) || empty($email) || empty($password) || empty($passwordRepeat)){
-    header("Location: /var/www/html/signup.php?error=emptyfields&uid=".$username."&mail=".$email);
+    require '../signup.php';
     exit();
   }
   else if (){
