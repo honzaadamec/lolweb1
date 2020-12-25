e5f5c07e01420285204aa148b9edadc6c7b59e05
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-25 17:21:01 +0100

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -45,7 +45,7 @@ if (isset($_POST['signup-submit'])){
         exit();
       }
       else{
-        $sql = "INSERT INTO user (uidUsers, emailUser, pwdUsers) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
           header("Location: ../signup.php?error=sqlerror2");
