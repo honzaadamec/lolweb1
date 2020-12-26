7f49bea8014e323464fdfce646fd575cc3f22085
honzaadamec <honza.adamec00@gmail.com>
2020-12-26 10:00:02 +0100

    nice

--- index.php
+++ index.php
@@ -3,8 +3,14 @@
  ?>
 
       <main>
-        <p>Jsi odhlasen</p>
-        <p>Jsi prihlasen</p>
+        <?php
+          if (isset($_SESSION['userId'])) {
+            echo '<p>Jsi odhlasen</p>';
+          }
+          else {
+            echo '<p>Jsi prihlasen</p>';
+          }
+         ?>
       </main>
 
  <?php
