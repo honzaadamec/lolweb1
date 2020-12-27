c72edb4b8e11998f15744860b23d03bd5d385852
honzaadamec <76455179+honzaadamec@users.noreply.github.com>
2020-12-27 03:06:11 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -14,18 +14,24 @@
         <a href="#">
         </a>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About us</a></li>
-          <li><a href="#">Turnaje</a></li>
-          <li><a href="#">Kontakty</a></li>
+          <li><a href="index.php">Home</a></li>
         </ul>
         <div>
-          <form action="includes/login.inc.php" method="post">
-<!--post method je pro secure, je to cast kterou uzivatel nevidi, budou v nem scripty jakou jsou login logout atd..-->
-            <input type="text" name="mailuid" placeholder="E-mail...">
-            <input type="password" name="pwd" placeholder="Password...">
-            <button type="submit" name="login-submit">Login</button>
-          </form>
+          <?php
+              if (isset($_SESSION['userId'])) {
+                echo '<form action="includes/logout.inc.php" method="post">
+                  <button type="submit" name="logout-submit">Logout</button>
+                </form>';
+              }
+              else {
+                echo '<form action="includes/login.inc.php" method="post">
+                  <input type="text" name="mailuid" placeholder="E-mail...">
+                  <input type="password" name="pwd" placeholder="Password...">
+                  <button type="submit" name="login-submit">Login</button>
+                </form>
+              <a href="signup.php">Signup</a>';
+              }
+           ?>
         <a href="signup.php">Signup</a>
           <form action="includes/logout.inc.php" method="post">
             <button type="submit" name="logout-submit">Logout</button>
